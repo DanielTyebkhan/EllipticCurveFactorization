52cf792d5f1f344b89160b9d90b2857450f9cbc7
Daniel Tyebkhan <dtyebkhan@gmail.com>
2022-11-08 11:43:06 -0500

    implemented inverse

--- math_helpers.py
+++ math_helpers.py
@@ -3,9 +3,10 @@ import math
 
 def mult_inverse(number, modulus) -> Optional[int]:
     """
-    Returns x such that: number * x \equiv 1 mod modulus or none if no such x exists
+    Returns x such that: number * x % modulus = 1 
+        or None if no such x exists
     """
     if math.gcd(number, modulus) != 1:
         return None
-    return 
+    return pow(number, -1, modulus)
     
